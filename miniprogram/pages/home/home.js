1af6242c9155e68c7d2b0dce05db7536a67e856f
Yongfeng <kymtwyf@gmail.com>
2018-12-16 00:12:49 +0800

    init openid

--- miniprogram/pages/home/home.js
+++ miniprogram/pages/home/home.js
@@ -111,6 +111,18 @@ Page({
    */
   onLoad: function (options) {
     this.initRecord()
+
+    wx.cloud.callFunction({
+      name: 'login',
+      data: {},
+      success: res => {
+        console.log('[云函数] [login] user openid: ', res.result.openid)
+        app.globalData.openid = res.result.openid
+      },
+      fail: err => {
+        console.error('[云函数] [login] 调用失败', err)
+      }
+    })
   },
 
   /**
